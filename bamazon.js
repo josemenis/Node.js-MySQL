391a758f3b1bbc18bd483add2c81ecc157d96740
Little Milo <no-reply>
2019-05-15 21:26:18 -0700

    js file added & mySQL connection is OK, DB created

---
+++ bamazon.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql')
+var inquirer = require('inquirer')
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'nodeUser',
+
+  // Your password
+  password: '',
+  database: 'bamazon_DB'
+})
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err
+  // run the start function after the connection is made to prompt the user
+  start()
+})
+
+function start () {
+    inquirer
+      .prompt({
+        name: 'postOrBid',
+        type: 'list',
+        message: 'Would you like to [POST] an auction or [BID] on an auction?',
+        choices: ['POST', 'BID', 'EXIT']
+      })
+      .then(function (answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === 'POST') {
+          postAuction()
+        } else if (answer.postOrBid === 'BID') {
+          bidAuction()
+        } else {
+          connection.end()
+        }
+      })
+  }
