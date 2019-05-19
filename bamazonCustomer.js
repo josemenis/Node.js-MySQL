29fccd1209e7f1235cdc55ab36186cb51704519c
Little Milo <no-reply>
2019-05-19 13:14:30 -0700

    5 & 6 of instructions completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,36 +24,44 @@ connection.connect(function (err) {
 })
 
 function start() {
-  // This connection.query will select all products from the db and display through console log.
+  // This connection.query will select all products from the db 
   connection.query('SELECT * FROM products', function (err, res) {
     if (err) throw err
-    console.log(res)
-    prompt()
-  }
-  )}
-
-  // function to prompt to ask user
-  function prompt() {
-  inquirer
-    .prompt({
-      name: 'SKU',
-      type: 'list',
-      message: 'What is the SKU of the product that you would like to buy?',
-      // this validate function will ensure that the user can only input a number
-      validate: function (value) {
-        if (isNaN(value) === false) {
-          return true
+    // console.log(res)
+    inquirer
+      .prompt([
+        {
+          name: 'choice',
+          type: 'rawlist',
+          choices: function () {
+            // create an empty array
+            var choiceArray = []
+            for (var i = 0; i < res.length; i++) {
+              // pushes items into choicesArray
+              choiceArray.push(res[i].product_name)
+            }
+            return choiceArray
+          },
+          //    * The first should ask them the SKU of the product they would like to buy.
+          message: 'What is the SKU of the product that you would like to buy?',
+          // this validate function will ensure that the user can only input a number
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true
+            }
+            return false
+          }
+        },
+        {
+          //    * The second message should ask how many units of the product they would like to buy.
+          name: 'units',
+          type: 'input',
+          message: 'How many units would you like to buy?'
         }
-        return false
-      }
-    })
+      ])
   }
-//   5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the skus, names, and prices of products for sale.
-
-//   6. The app should then prompt users with two messages.
-
-//    * The first should ask them the SKU of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
+  )
+}
 
 // 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 
