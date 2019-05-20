09c6197c5dc3e277bcb21913a7f6397932aa0700
Little Milo <no-reply>
2019-05-19 18:01:51 -0700

    finished minimum requirement for the assignment

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-var mysql = require('mysql')
-var inquirer = require('inquirer')
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -14,14 +14,14 @@ var connection = mysql.createConnection({
   // Your password
   password: '',
   database: 'bamazon_DB'
-})
+});
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err
   // run the start function after the connection is made to display products
   start()
-})
+});
 
 function start() {
   // This connection.query will select all products from the db 
@@ -56,14 +56,66 @@ function start() {
           //    * The second message should ask how many units of the product they would like to buy.
           name: 'units',
           type: 'input',
-          message: 'How many units would you like to buy?'
+          message: 'How many units would you like to buy?',
+          // this validate function will ensure that the user can only input a number
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true
+            }
+            return false
+          }
         }
       ])
+      // asynchronous takes a while
+      .then(function (answer) {
+        // get the information of the chosen item
+        var chosenItem
+        for (var i = 0; i < res.length; i++) {
+          // console log below code to understand what objects are looping through, res [i] picks one at a time
+          // console.log("/////////////////////////////////////////")
+          // console.log(res[i])
+          if (res[i].product_name === answer.choice) {
+            chosenItem = res[i]
+          }
+        }
+        // console.log(chosenItem.stock_quantity)
+        // console.log(parseInt(answer.units))
+
+        // 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+        if (chosenItem.stock_quantity > parseInt(answer.units)) {
+          // 
+          var remainingStock = chosenItem.stock_quantity - parseInt(answer.units)
+          // console.log(remainingStock)
+          connection.query(
+
+            'UPDATE products Set ? WHERE ?',
+            // 1 object per question mark
+            [
+              {
+                // column name for this instance it is the key : variable
+                stock_quantity: remainingStock
+              },
+              {
+                product_name: chosenItem.product_name
+              }
+            ]
+          )
+          var totalPrice = parseInt(answer.units) * chosenItem.price
+          console.log(`Your total is ${totalPrice}`)
+          console.log('Thanks for shopping!')
+          
+          // method that comes with mySQL, method is a function that belongs to a object.
+          connection.end ()
+
+        } else {
+          console.log("Sorry not enough stock'")
+          start()
+        }
+      })
   }
   )
-}
+};
 
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 
 //    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
 
