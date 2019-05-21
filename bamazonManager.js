342c4660fcfeb36311c5d110641ebc6689ddfcf2
Little Milo <no-reply>
2019-05-20 23:53:10 -0700

    last functon is a doozy

--- bamazonManager.js
+++ bamazonManager.js
@@ -203,64 +203,85 @@ function updateInventory() {
 };
 //     * Add New Product
 function addNewProduct() {
-    connection.connect(function (err) {
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err
-    // prompt question for new product
-    inquirer
-    .prompt([
-      {
-    name: 'sku',
-    type: 'input',
-    message: 'Enter a SKU:',
-    // this validate function will ensure that the user can only input a number
-    validate: function (value) {
-        if (isNaN(value) === false) {
-            return true
-        }
-        return false
-    }
-      },
-      {
-    name: 'product',
-    type: 'input',
-    message: 'What is the product name?'
-      },
-      {
-    name: 'department',
-    type: 'input',
-    message: 'Enter a department for this product.'
-      },
-      {
-    name: 'price',
-    type: 'input',
-    message: 'What is the cost?',
-    // this validate function will ensure that the user can only input a number
-    validate: function (value) {
-        if (isNaN(value) === false) {
-            return true
-        }
-        return false
-    }
-      },
-      {
-    name: 'quantity',
-    type: 'input',
-    message: 'Enter the quantity for this product.',
-    // this validate function will ensure that the user can only input a number
-    validate: function (value) {
-        if (isNaN(value) === false) {
-            return true
-        }
-        return false
-    }
-      }
-    ])
-    .then(answers => {
-        console.log(answers)
-      // Use user feedback for... whatever!!
-    });
-    // then insert into db
+        // prompt question for new product
+        inquirer
+            .prompt([
+                {
+                    name: 'sku',
+                    type: 'input',
+                    message: 'Enter a SKU:',
+                    // this validate function will ensure that the user can only input a number
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true
+                        }
+                        return false
+                    }
+                },
+                {
+                    name: 'product',
+                    type: 'input',
+                    message: 'What is the product name?'
+                },
+                {
+                    name: 'department',
+                    type: 'input',
+                    message: 'Enter a department for this product.'
+                },
+                {
+                    name: 'price',
+                    type: 'input',
+                    message: 'What is the cost?',
+                    // this validate function will ensure that the user can only input a number
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true
+                        }
+                        return false
+                    }
+                },
+                {
+                    name: 'quantity',
+                    type: 'input',
+                    message: 'Enter the quantity for this product.',
+                    // this validate function will ensure that the user can only input a number
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true
+                        }
+                        return false
+                    }
+                }
+            ])
+            .then(answers => {
+                // prompt works fine
+                // console.log(answers.sku, answers.product, answers.department, answers.price,answers.quantity)
+                var query = connection.query(
+                'INSERT INTO products SET ?', [
+                    {
+                        sku : answers.sku
+                    },
+                    {
+                        product_name : answers.product
+                    },
+                    {
+                        department_name : answers.department
+                    },
+                    {
+                        price : answers.price
+                    },
+                    {
+                        stock_quantity : answers.quantity
+                    }
+                ])
+                console.log(res.affectedRows + ' product inserted!\n')
+                // connection.end()
+            });
+        // then insert into db
     })
+    console.log(query.sql)
 };
 
 //   * If a manager selects `View Products for Sale`, the app should list every available item: the item SKUs, names, prices, and quantities.
