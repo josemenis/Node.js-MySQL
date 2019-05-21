161a6563581308e2cb4666cbf01aac9b0932528b
Little Milo <no-reply>
2019-05-20 20:26:07 -0700

    working on manager.js if/else and running functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,10 +21,40 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err
     // run the start function after the connection is made to display products
-    displayItem()
+    // productsForSale()
+    // lowInventory()
+    inquirer
+        .prompt([{
+            /* Pass your questions in here */
+            name: 'command',
+            message: 'What would you like to do?',
+            type: 'list',
+            choices: menuItems
+        }])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            // console.log(answers)
+            if (answers === menuItems[0]) {
+                productsForSale()
+            } else if (answers === menuItems[1]) {
+                lowInventory()
+            } else if (answers === menuItems[2]) {
+                updateInventory()
+            } else if (answers === menuItems[3]) {
+                addNewProduct()
+            }
+        });
 });
 
-function displayItem() {
+// ### Challenge #2: Manager View (Next Level)
+
+// * Create a new Node application called `bamazonManager.js`. Running this application will:
+
+//   * List a set of menu options:
+let menuItems = ['View Products for Sale', 'View Low Inventory', 'Update Inventory', 'Add New Product']
+
+//     * View Products for Sale
+function productsForSale() {
     [
         connection.query('SELECT * FROM products', function (err, res) {
             if (err) throw err
@@ -34,20 +64,68 @@ function displayItem() {
             })
             for (var i = 0; i < res.length; i++) {
                 // pushes items into choicesArray
-                let inventoryItem =[
-                   sku = res[i].sku,
-                   productname = res[i].product_name,
-                   price = res[i].price,
-                   departmentname = res[i].department_name,
-                   stockquantity = res[i].stock_quantity
+                let inventoryItem = [
+                    sku = res[i].sku,
+                    productname = res[i].product_name,
+                    price = res[i].price,
+                    departmentname = res[i].department_name,
+                    stockquantity = res[i].stock_quantity
                 ]
-                    table.push([`${sku}`,
-                    `${productname}`,
-                    `${price}`,
-                    `${departmentname}`,
-                    `${stockquantity}`]);
-                }
-                console.log(table.toString());
+                table.push([`${sku}`,
+                `${productname}`,
+                `${price}`,
+                `${departmentname}`,
+                `${stockquantity}`]);
+            }
+            console.log(table.toString());
+            connection.end()
         })
     ]
 };
+
+
+//     * View Low Inventory
+function lowInventory() {
+    [
+        // From DB products WHERE stock is less than 5
+        connection.query('SELECT * FROM products WHERE stock_quantity < 5', function (err, res) {
+            if (err) throw err
+            // npm cli table
+            let table = new Table({
+                head: ['SKU', 'Product', 'Price', 'Department', 'Quantity']
+            })
+            for (var i = 0; i < res.length; i++) {
+                // pushes items into choicesArray
+                let inventoryItem = [
+                    sku = res[i].sku,
+                    productname = res[i].product_name,
+                    price = res[i].price,
+                    departmentname = res[i].department_name,
+                    stockquantity = res[i].stock_quantity
+                ]
+                table.push([`${sku}`,
+                `${productname}`,
+                `${price}`,
+                `${departmentname}`,
+                `${stockquantity}`]);
+            }
+            // log per documentation
+            console.log(table.toString());
+            // end connection
+            connection.end()
+        })
+    ]
+};
+//     if statement quantity <5, run a loop
+
+//     * Add to Inventory
+
+//     * Add New Product
+
+//   * If a manager selects `View Products for Sale`, the app should list every available item: the item SKUs, names, prices, and quantities.
+
+//   * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
+
+//   * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+
+//   * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
